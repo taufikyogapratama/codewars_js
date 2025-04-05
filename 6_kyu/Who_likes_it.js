e022c1a402f55c724b3232eee0d3af82f4cf2d15
Taufik <164930441+TaufikYoga@users.noreply.github.com>
2025-04-05 13:48:09 +0700

    Who likes it?

---
+++ 6_kyu/Who_likes_it.js
@@ -0,0 +1,15 @@
+const likes = names => {
+    if(names.length >=4){
+        return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+    }else if(names.length === 3){
+        return `${names[0]}, ${names[1]} and ${names[2]} like this`
+    }else if(names.length === 2){
+        return `${names[0]} and ${names[1]} like this`
+    }else if(names.length === 1){
+        return `${names[0]} likes this`
+    }else{
+        return "no one likes this"
+    }
+}
+
+console.log(likes(["Alex", "Jacob", "Mark", "Max", "Anjay"]))
